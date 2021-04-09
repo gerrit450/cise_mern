f77bf810cb10108cd9943a0f79b8e9873353d778
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-04-09 18:28:07 +1200

    added states to change front based on connection status

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import connection from './connect.js';
 
-function App() {
+class App extends Component {
+
+  state = {txt: "Welcome!"};
+
+  render()
+  {
+  var conn = new connection(); //anonymous object to connect the server
+  this.state.txt = conn.runServer(); // returns a string based on connection status
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.txt}
         </p>
         <a
           className="App-link"
@@ -20,6 +29,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
