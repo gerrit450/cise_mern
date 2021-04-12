d8d50ca660f34799b218013659925e8ebca8dd85
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-04-13 10:04:59 +1200

    deleting book by entering title of book

---
+++ src/deleteBook.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react';
+import styles from './index.css';
+import Submitted from './submitted.js';
+
+class deleteBook extends Component {
+
+}
+
+export default deleteBook;
