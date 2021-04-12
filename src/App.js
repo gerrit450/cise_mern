0f6ffefd2a737c397eb5df2e437a6f9eca0440bb
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-04-13 09:56:36 +1200

    changed homescreen to four buttons for CRUD

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import {Component} from 'react';
-import connection from './connect.js';
+import {Button} from 'react-bootstrap'; 
+import history from './history';
+import { Link} from "react-router-dom"; //used to navigate to a new js component
+
 
 class App extends Component {
 
-  state = {txt: "Welcome!"};
-
   render()
   {
-  var conn = new connection(); //anonymous object to connect the server
-  this.state.txt = conn.runServer(); // returns a string based on connection status
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.txt}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+          <Button onClick={() => history.push('./createBook')}>Insert new book</Button> 
+          </form>
+          <Button onClick={() => history.push('./readBook')}>Read existing books</Button>
+          <Button onClick={() => history.push('./updateBook')}>Update an existing book</Button>
+          <Button onClick={() => history.push('./deleteBook')}>delete a book</Button>
     </div>
   );
   }
